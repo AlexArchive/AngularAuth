a01ebb98d7ff68d446713493942ee487c96f317c
ByteBlast <byteblast@gmail.com>
2015-02-01 15:31:49 +0000

    Added register api

---
+++ AngularAuth/Authentication/AuthenticationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AngularAuth.Authentication
+{
+    public class AuthenticationContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthenticationContext() : base ("AuthContext")
+        {
+        }
+    }
+}
---
+++ AngularAuth/Authentication/AuthenticationRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using AngularAuth.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AngularAuth.Authentication
+{
+    public class AuthRepository : IDisposable
+    {
+        private AuthenticationContext _ctx;
+
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _ctx = new AuthenticationContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public async Task<IdentityResult> RegisterUser(User model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+            _userManager.Dispose();
+
+        }
+    }
+}
