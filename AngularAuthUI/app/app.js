007fc1e928eaaeedc8b02881de5ab8207725b4d0
ByteBlast <byteblast@gmail.com>
2015-02-02 07:37:55 +0000

    Added Angular dependencies and app script

---
+++ AngularAuthUI/app/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('AngularAuth', ['ngRoute', 'LocalStorageModule']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
